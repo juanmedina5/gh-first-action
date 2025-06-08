d0a870834bf6fc797137e38f560383bb2c9fe600
Juan Manuel Medina Baron <juan.x.medina.-nd@disney.com>
2025-06-08 16:50:43 -0500

    Test Docker Actions

--- .github/actions/docker-actions/greet.py
+++ .github/actions/docker-actions/greet.py
@@ -3,7 +3,7 @@ import os
 def run():
     name = os.environ['INPUT_NAME']
 
-    print('Saludando desde Docker Actions a' + name)
+    print('Saludando desde Docker Actions a ' + name)
 
 if __name__ == '__main__':
     run()
