0c65861061ed142ca737d7e9fdcbcfdb9f9778d0
Juan Manuel Medina Baron <juan.x.medina.-nd@disney.com>
2025-06-08 16:15:32 -0500

    Test JS Actions

--- .github/actions/js-actions/main.js
+++ .github/actions/js-actions/main.js
@@ -4,5 +4,7 @@ const exec=require('@actions/exec');
 
 function run(){
     core.notice('Ejecutando Desde JS Actions!');
+    const name = core.getInput('name',{required: true});
+    core.notice('Enviando un saludo a', $name);
 }
 run();
